eb505205f99f74bf3f929d3033748848bc7be4b5
Gavtriple9 <29155514+Gavtriple9@users.noreply.github.com>
2023-10-15 19:36:17 -0700

    bbox tests pass

--- src/bbox.py
+++ src/bbox.py
@@ -1,10 +1,10 @@
 class BoundingBox:
-    def __init__(self):
-        self.x = 0.0
-        self.y = 0.0
-        self.w = 0.0
-        self.h = 0.0
-        self.c = 0.0
+    def __init__(self, x, y, w, h, c):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.c = c
 
     def __str__(self):
         return f"({self.x}, {self.y}, {self.w}, {self.h}, {self.c})"
@@ -55,19 +55,10 @@ class BoundingBox:
         Returns:
             BoundingBox: the union of this bounding box and another bounding box
         """
-        area = 0.0
         if self.intersects(other):
-            top_left = (
-                min(self.get_top_left()[0], other.get_top_left()[0]),
-                min(self.get_top_left()[1], other.get_top_left()[1]),
-            )
-            bottom_right = (
-                max(self.get_bottom_right()[0], other.get_bottom_right()[0]),
-                max(self.get_bottom_right()[1], other.get_bottom_right()[1]),
-            )
-            area = (bottom_right[0] - top_left[0]) * (bottom_right[1] - top_left[1])
+            return self.get_area() + other.get_area() - self.intrsect_area(other)
         else:
-            area = self.get_area() + other.get_area()
+            return self.get_area() + other.get_area()
 
     def intrsect_area(self, other: "BoundingBox"):
         """returns the intersection of this bounding box and another bounding box
@@ -78,7 +69,6 @@ class BoundingBox:
         Returns:
             BoundingBox: the intersection of this bounding box and another bounding box
         """
-        area = 0.0
         if self.intersects(other):
             top_left = (
                 max(self.get_top_left()[0], other.get_top_left()[0]),
@@ -88,8 +78,9 @@ class BoundingBox:
                 min(self.get_bottom_right()[0], other.get_bottom_right()[0]),
                 min(self.get_bottom_right()[1], other.get_bottom_right()[1]),
             )
-            area = (bottom_right[0] - top_left[0]) * (bottom_right[1] - top_left[1])
-        return area
+            return (bottom_right[0] - top_left[0]) * (bottom_right[1] - top_left[1])
+        else:
+            return 0.0
 
     def iou(self, other: "BoundingBox"):
         """returns the intersection over union of this bounding box and another bounding box
